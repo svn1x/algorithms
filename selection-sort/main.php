419640958eabaa83ed3240841d5a4042c52d43d8
svn1x <svn1x@outlook.com>
2023-08-04 18:55:31 +0600

    Added and removed newlines.

--- selection-sort/main.php
+++ selection-sort/main.php
@@ -3,10 +3,13 @@
 function selectionSort(array $numbers): array
 {
     $length = count($numbers);
+
     for ($i = 0; $i < $length - 1; $i++) {
+
         $minIndex = $i;
 
         for ($j = $i + 1; $j < $length; $j++) {
+            
             if ($numbers[$j] < $numbers[$minIndex]) {
                 $minIndex = $j;
             }
@@ -20,6 +23,5 @@ function selectionSort(array $numbers): array
 
     return $numbers;
 }
-
 
 print_r(selectionSort([9, 5, 7, 6, 1, 2, 4, 0, 3, 8]));
