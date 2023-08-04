ca70a20a7d40c8a0c0dffc6fc97d154cab1be558
svn1x <svn1x@outlook.com>
2023-08-04 18:45:05 +0600

    Selection Sort

---
+++ selection-sort/main.php
@@ -0,0 +1,25 @@
+<?php
+
+function selectionSort(array $numbers)
+{
+    $length = count($numbers);
+    for ($i = 0; $i < $length - 1; $i++) {
+        $minIndex = $i;
+
+        for ($j = $i + 1; $j < $length; $j++) {
+            if ($numbers[$j] < $numbers[$minIndex]) {
+                $minIndex = $j;
+            }
+        }
+
+        $temp = $numbers[$i];
+        $numbers[$i] = $numbers[$minIndex];
+        $numbers[$minIndex] = $temp;
+
+    }
+
+    return $numbers;
+}
+
+
+print_r(selectionSort([9, 5, 7, 6, 1, 2, 4, 0, 3, 8]));
