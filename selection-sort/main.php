91d18edc38dde80462115011fb5907c01bdb4caa
svn1x <svn1x@outlook.com>
2023-08-18 21:54:24 +0600

    Upgraded

--- selection-sort/main.php
+++ selection-sort/main.php
@@ -12,9 +12,13 @@ function selectionSort(array $numbers)
             }
         }
 
-        $temp = $numbers[$i];
-        $numbers[$i] = $numbers[$minIndex];
-        $numbers[$minIndex] = $temp;
+        // Old way:
+        // $temp = $numbers[$i];
+        // $numbers[$i] = $numbers[$minIndex];
+        // $numbers[$minIndex] = $temp;
+
+        // New way:
+        [$numbers[$i], $numbers[$minIndex]] = [$numbers[$minIndex], $numbers[$i]];
 
     }
 
